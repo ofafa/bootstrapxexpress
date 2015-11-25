dc739e8750fd86490f45badb08f1c23a65ef539c
ofafa <chunghsianghsueh@gmail.com>
2015-11-26 01:07:24 +0800

    Finished ch1.6 Comment [1] Add comment.js and comment.ejs [2] Add post route in index.js for comment function

--- models/post.js
+++ models/post.js
@@ -30,7 +30,8 @@ Post.prototype.save = function(callback){
         name: this.name,
         time: time,
         title: this.title,
-        post: this.post
+        post: this.post,
+        comments: []
     };
 
     //connecting DB
@@ -110,7 +111,14 @@ Post.getOne = function(name, day, title, callback){
                 if(err){
                     return callback(err);
                 }
-                doc.post = markdown.toHTML(doc.post);
+                if(doc){
+                    doc.post = markdown.toHTML(doc.post);
+                    doc.comments.forEach(function(comment){
+                        comment.content = markdown.toHTML(comment.content);
+                    });
+                }
+
+
                 callback(null, doc);
             });
 
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ var multer = require('multer');
 
 User = require('../models/user');
 Post = require('../models/post');
+Comment = require('../models/comment');
 
 router.use(session({
     secret: settings.cookieSecret,
@@ -295,6 +296,29 @@ router.get('/remove/:name/:day/:title', function(req, res){
     });
 });
 
+
+//post comments
+router.post('/u/:name/:day/:title', function(req, res){
+    var date = new Date();
+    time = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '' + date.getHours() + ':' + (date.getMinutes() < 10? '0' + date.getMinutes() : date.getMinutes());
+    var comment = {
+        name: req.body.name,
+        email: req.body.email,
+        website: req.body.website,
+        time: time,
+        content: req.body.content
+    };
+
+    var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+    newComment.save(function (err){
+        if(err){
+            req.flash('error', err);
+            return res.redirece('back');
+        }
+        req.flash('success', 'comment left successfully!');
+        res.redirect('back');
+    });
+});
 
 /* Utility function */
 function checkLogin(req, res, next){
